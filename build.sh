0b40b73042bdb81e25a47a9230e9d84ee108c8ea
flinti <git@flinti.de>
2025-02-07 00:34:08 +0100

    shader build via cmake

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-glslc -o data/shader/shader.frag.spv -fshader-stage=fragment data/shader.frag.glsl && \
-glslc -o data/shader/shader.vert.spv -fshader-stage=vertex data/shader.vert.glsl && \
 cmake -S . -B build && cmake --build build
